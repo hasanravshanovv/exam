cff7a7c8d66b430814a4ccd32e9cebfcd8ab36c1
hasanravshanovv <ravshanovhasana@gmail.com>
2023-02-04 15:04:11 +0300

    added

---
+++ index.py
@@ -0,0 +1,21 @@
+# day = int(input("enter days - "))
+# while day - 365 > 0:
+#     print(day, 1)
+#     day -= 365
+
+
+# sentense = str(input("enter a sentence - "))
+# sentence_2 = str(input("enter a searching word - "))
+# while sentense == sentence_2:
+#     {
+#         print(sentence_2)
+#     }
+
+
+sentence = str(input("enterv a sentence - "))
+s = str(input("enterv a searching - "))
+if s in sentence:
+    print(s)
+else:
+
+    print("error")
